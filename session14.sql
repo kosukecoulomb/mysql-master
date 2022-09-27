e50175c6ec14b8080ca09c6cbbf753a06e59d179
kosukecoulomb <jobch.coulomb@gmail.com>
2022-09-27 19:23:18 +0900

    [practice]サブクエリ

--- session14.sql
+++ session14.sql
@@ -188,3 +188,101 @@ WHERE
             products
         WHERE
             id < 5);
+
+
+SELECT * FROM product_reviews;
+
+SELECT user_id FROM product_reviews WHERE product_id = 5;
+
+SELECT 
+    *
+FROM
+    users
+WHERE
+    id IN (SELECT 
+            user_id
+        FROM
+            product_reviews
+        WHERE
+            product_id = 5)
+ORDER BY users.id;
+
+
+SELECT 
+    DISTINCT users.*
+FROM
+    users
+		INNER JOIN
+	product_reviews ON product_reviews.user_id = users.id
+WHERE
+	product_id = 5;
+    
+
+SELECT 
+    AVG(sub.sum), MIN(sub.sum), MAX(sub.sum)
+FROM
+    (SELECT 
+        gender, SUM(amount) AS sum
+    FROM
+        order_details
+    INNER JOIN orders ON orders.id = order_details.order_id
+    INNER JOIN users ON users.id = orders.user_id
+    GROUP BY users.gender) AS sub;
+
+
+SELECT 
+    *
+FROM
+    users
+WHERE
+    EXISTS( SELECT 
+            *
+        FROM
+            product_reviews
+        WHERE
+            product_id = 7
+                AND body LIKE '%disappointing%');
+
+
+SELECT 
+    *
+FROM
+    users
+WHERE
+    id IN(SELECT 
+            user_id
+        FROM
+            product_reviews
+        WHERE
+            product_id = 7
+                AND body LIKE '%disappointing%');
+
+
+SELECT * FROM order_details;
+
+
+SELECT 
+    users.id,
+    users.name,
+    users.email,
+    users.gender,
+    users.birthday,
+    SUM(products.price * order_details.amount) AS 'total price',
+    SUM(products.price * order_details.amount * 1.10) AS 'tax include',
+    COUNT(order_details.order_id) AS order_count
+FROM
+    users
+        LEFT JOIN
+    orders ON users.id = orders.user_id
+        INNER JOIN
+    order_details ON orders.id = order_details.order_id
+        INNER JOIN
+    products ON order_details.product_id = products.id
+GROUP BY orders.user_id
+HAVING SUM(amount * price) < (SELECT 
+        SUM(price)
+    FROM
+        products)
+ORDER BY users.id;
+
+
